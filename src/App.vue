17e4eea92c7c56bbdacd6360f5a87efbb612a83d
Reggie <inzscript@gmail.com>
2021-07-01 16:09:59 -0700

    Binding Classes Array

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
     <div v-html="channel"></div>
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 class="underline">Underlined Text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? Movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Condition Movie</h2> 
+    <h2 v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldout,
+      'sold-out': isSoldout,
+      }">Object Conditional Movie</h2>
   </div>
 </template>
 
@@ -16,6 +27,9 @@ export default {
       channel: "<strong>Codevolution</strong>",
       headingId: "heading",
       isDisabled: true,
+      status: "danger",
+      isPromoted: true,
+      isSoldout: true,
     };
   },
 }
@@ -30,4 +44,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.underline {
+  text-decoration: underline;
+}
+
+ .promoted {
+   font-style: italic;
+   color: orange;
+ }
+
+ .new {
+   color: olivedrab
+ }
+
+ .sold-out {
+   color: red; 
+ }
+
 </style>
