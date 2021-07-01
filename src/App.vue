3974c140a8bfc995fe378550186a5c2def48090d
Reggie <inzscript@gmail.com>
2021-07-01 15:24:38 -0700

    Binding Text

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Hello {{ name }}</div>
+  <div v-text="channel"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      name: "Batman",
+      channel: "Codevolution",
+    };
+  },
 }
 </script>
 
