6887a589f7b05d10767b4f28008ebb524bd9f4b5
Reggie <inzscript@gmail.com>
2021-07-02 14:50:23 -0700

    Change to v-bind shorthand

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,28 @@
   <div>
     <div>Hello {{ name }}</div>
     <div v-html="channel"></div>
-    <h2 v-bind:id="headingId">Heading</h2>
-    <button v-bind:disabled="isDisabled">Bind</button>
+    <h2 :id="headingId">Heading</h2>
+    <button :disabled="isDisabled">Bind</button>
     <h2 class="underline">Underlined Text</h2>
-    <h2 class="underline" v-bind:class="status">Status</h2>
-    <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-    <h2 v-bind:class="isSoldout ? 'sold-out' : 'new'">Soldout? Movie</h2>
-    <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
-    <h2 v-bind:class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Condition Movie</h2> 
-    <h2 v-bind:class="{
+    <h2 class="underline" :class="status">Status</h2>
+    <h2 :class="isPromoted && 'promoted'">Promoted Movie</h2>
+    <h2 :class="isSoldout ? 'sold-out' : 'new'">Soldout? Movie</h2>
+    <h2 :class="['new', 'promoted']">Newly Promoted Movie</h2>
+    <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">Array Condition Movie</h2> 
+    <h2 :class="{
       promoted: isPromoted,
       new: !isSoldout,
       'sold-out': isSoldout,
       }">Object Conditional Movie</h2>
-      <h2 v-bind:style="{
+      <h2 :style="{
         color: highlightColor,
         'font-size': headerSize + 'px',
         fontSize: headerSize + 'px',
         padding: '20px',
       }">Inline Style</h2>
-      <h2 v-bind:style="headerStyleObject">Style Object</h2>
-      <div v-bind:style="[baseStyleObject, successStyleObject]">Success Style</div>
-      <div v-bind:style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+      <h2 :style="headerStyleObject">Style Object</h2>
+      <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
+      <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
   </div>
 </template>
 
